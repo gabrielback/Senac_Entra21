203445b253aaf80a4705888f58a58ca94fee5781
Gabriel-Gramaq <gabrielluiz-back@hotmail.com>
2022-06-14 13:01:15 -0300

    Atualização HTMLCSS

--- HTML_CSS/projetoFinalHTML_CSS/js/index.js
+++ HTML_CSS/projetoFinalHTML_CSS/js/index.js
@@ -19,3 +19,19 @@ function openPage(element, url) {
     xml.open("GET", url, true)
     xml.send()
 }
+
+
+        // // Jquery é universalmente referenciado com o dólar "$"
+        // $('div').hide(1000).show(5000).fadeOut(5000) // seletor
+        // console.log($('div').get(0))
+        // console.log($.isEmptyObject({nome: null}))
+        // console.log($.isEmptyObject({}))
+        // console.log(jQuery.inArray(3, [1, 2, 3]))
+        // const array1 = [55, 44, 33] // retorna posição do array, caso não esteja presente, retorna -1
+        // console.log($.inArray(44, array1))
+        // console.log($.inArray(45, array1)) // retorna -1. Não está presente no array.
+        // $.fn.bgColor = function (){
+        //     this.css('background-color', 'green')
+        //     return this
+        // }
+        // $('body').bgColor().fadeOut(1000)

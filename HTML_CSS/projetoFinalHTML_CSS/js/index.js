e2c14d3531cec57357fcb3b7f4d27be6b20f2eb7
Gabriel Back <gabrielback12@gmail.com>
2022-06-13 22:25:35 -0300

    Atualização HTML CSS

--- HTML_CSS/projetoFinalHTML_CSS/js/index.js
+++ HTML_CSS/projetoFinalHTML_CSS/js/index.js
@@ -1,20 +1,28 @@
-let mainArticle = document.getElementById('mainArticle')
-mainArticle.innerHTML = openPage('mainArticle', 'main.html')
+let mainArticle = document.getElementById('artigo')
+artigo.innerHTML = openPage('artigo', 'main.html')
 
-// document.onload(openPage('mainArticle', 'main.html'))
+const selectElement = () => {
+    let li = document.querySelectorAll('ul>li>a')
+    
+    li.forEach(element => {
+        element.addEventListener('click', () => {
+            let selectedElement = document.querySelector('.selected')
+            selectedElement.classList.remove('selected')
+            element.classList.add('selected')
+        })
+    })
+}
 
+// selectElement()
 
-function openPage(target, url) {
-    url = `./src/${url}`
+function openPage(element, url) {
+    url = `./pages/${url}`
     var xml = new XMLHttpRequest()
     xml.onreadystatechange = function () {
-        console.log(xml)
         if (xml.readyState == 4 && xml.status == 200) {
-            document.getElementById(target).innerHTML = xml.responseText
+            document.getElementById(element).innerHTML = xml.responseText
         }
     }
     xml.open("GET", url, true)
-
     xml.send()
 }
-

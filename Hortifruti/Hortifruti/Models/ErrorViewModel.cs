56cca1daec4c1ed8160ae880ee3623f8a47a89be
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-09-14 00:07:13 -0300

    Organizando Diretório

--- Hortifruti/Hortifruti/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hortifruti.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
